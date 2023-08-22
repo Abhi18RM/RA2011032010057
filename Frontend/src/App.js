18d060b985bc855429ed4dea4e4c2e9fb4dfc1f4
Abhiram Ravuri <101940348+Abhi18RM@users.noreply.github.com>
2023-08-23 00:40:03 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TrainStatus from "./pages/TrainStatus";
+import axios from "axios";
 
 export default function App() {
+    const [trainData, setTrainData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const reqBody = {
+                    companyName: "Train Central",
+                    clientID: "d81d8dfa-9dc7-4c06-bc1c-4c44399ce165",
+                    clientSecret: "zubklHSaxZVdHppA",
+                    ownerName: "Abhiram",
+                    ownerEmail: "rv2890@srmist.edu.in",
+                    rollNo: "RA2011032010057",
+                };
+
+                const response = await axios.post(
+                    "http://20.244.56.144/train/auth",
+                    reqBody
+                );
+                const headers = {
+                    Authorization: `Bearer ${response.data.access_token}`,
+                };
+                const res = await axios.get(
+                    "http://20.244.56.144/train/trains",
+                    {
+                        headers,
+                    }
+                );
+                setTrainData(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    console.log(trainData);
+
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/train/:trainNumber" element={<TrainStatus />} />
+                <Route path="/" element={<Home data={trainData} />} />
+                <Route
+                    path="/train/:trainNumber"
+                    element={<TrainStatus data={trainData} />}
+                />
             </Routes>
         </Router>
     );
